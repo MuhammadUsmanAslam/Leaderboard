7a04f7abb6ebbd85faf12e0bf0fe3116102e9b67
MuhammadUsmanAslam <musmanaslam520@gmail.com>
2023-03-08 23:39:27 +0500

    Add API CRUD operations

--- src/index.js
+++ src/index.js
@@ -2,46 +2,35 @@ import addScore from './modules/handle-score.js';
 
 import './style.css';
 
-const scores = [
-  {
-    name: 'Muhammad',
-    score: 10,
-  },
-  {
-    name: 'Usman',
-    score: 20,
-  },
-  {
-    name: 'Aslam',
-    score: 30,
-  },
-];
-
-const displayScore = (scores) => {
+const displayScore = async () => {
   const scoreList = document.getElementById('score-list');
   scoreList.innerHTML = '';
-  scores.forEach((scoreListItem) => {
-    const scoreItem = document.createElement('li');
-    scoreItem.className = 'scrore-list-item';
-    scoreItem.innerHTML = `Name: ${scoreListItem.name}`;
-    scoreList.appendChild(scoreItem);
-  });
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yAPvdugADNNsXSbzR52S/scores/').then((response) => response.json()).then((json) => { 
+      let scores = json.result;
+      scores.forEach((scoreListItem) => {
+        const scoreItem = document.createElement('li');
+        scoreItem.className = 'scrore-list-item';
+        scoreItem.innerHTML = `Name: ${scoreListItem.user}, Score: ${scoreListItem.score}`;
+        scoreList.appendChild(scoreItem);
+      });
+    });
 };
 
 window.addEventListener('load', () => {
-  displayScore(scores);
+  displayScore();
   const refresh = document.getElementById('refresh');
   refresh.addEventListener('click', () => {
-    displayScore(scores);
+    displayScore();
   });
   const addNewScore = document.getElementById('add-new-score');
   addNewScore.addEventListener('click', (e) => {
     e.preventDefault();
     const inputName = document.getElementById('input-name');
     const inputScore = document.getElementById('input-score');
-    addScore(inputName.value, inputScore.value, scores);
+    addScore(inputName.value, inputScore.value);
     inputName.value = '';
     inputScore.value = '';
-    displayScore(scores);
+    displayScore();
   });
 });
--- src/modules/handle-score.js
+++ src/modules/handle-score.js
@@ -1,9 +1,17 @@
-const addScore = (name, score, scoreList) => {
-  const newScoreItem = {
-    name,
-    score,
-  };
-  scoreList.push(newScoreItem);
+const addScore = async (name, score) => {
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/yAPvdugADNNsXSbzR52S/scores/',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        user: name,
+        score,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    },
+  ).then((response) => response.json());
 };
 
 export default addScore;
